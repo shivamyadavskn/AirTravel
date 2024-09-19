3642b9b9b48306ee41e0ee5994074f52d99d2434
SHIVAM YADAV <demo@gmail.com>
2024-09-20 01:35:40 +0530

    tesing

--- src/components/searched/search.js
+++ src/components/searched/search.js
@@ -1,50 +1,5 @@
-"use client";
-import { useState, useEffect } from "react";
-import { FetchFlightsDataAPI } from "../services/flightSearch/FlightSearch";
-import FlightList from "../common/FlightListing/FlightList";
-import { useSearchParams } from "next/navigation";
-
-const Searched = () => {
-  const searchParams = useSearchParams();
-
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await FetchFlightsDataAPI(
-          sourceAirportCode,
-          destinationAirportCode,
-          itineraryType,
-          sortOrder,
-          numAdults,
-          numChildren,
-          numInfants,
-          classOfService,
-          pageNumber,
-          nearby,
-          nonstop,
-          currencyCode,
-          region,
-          date,
-          returnDate
-        );
-        setFlightsData(response.data);
-        setLoading(false);
-      } catch (error) {
-        setError(error.message);
-        setLoading(false);
-      }
-    };
-    fetchData();
-  }, []);
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error: {error}</p>;
+export default function Page() {
   return (
-    <div>
-      <FlightList flightsData={flightsData} />
-    </div>
+    <div>fdsf</div>
   );
-};
-
-export default Searched;
+}
